a327fe9ac2ea94b6af1fdf2fb48dd82717498838
aboodJS <aboodalsawafeen@gmail.com>
2026-03-21 19:48:02 +0300

    finished chapter 2

---
+++ firstscript.sh
@@ -0,0 +1,12 @@
+echo what is your name
+read MY_NAME
+echo "hello, and welcome $MY_NAME"
+echo "creating a file called $MY_NAME"
+
+touch "$ {
+  MY_NAME
+}_file.txt"
+echo "file created for $MY_NAME" > "$ {
+  MY_NAME
+}_file.txt"
+echo "done"
