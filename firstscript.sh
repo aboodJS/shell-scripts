229c2b4c5cc21a394b4c38f4004398e7518e4b9f
ajs <140010746+aboodJS@users.noreply.github.com>
2026-03-21 22:29:02 +0300

    fixed the naming issue

--- firstscript.sh
+++ firstscript.sh
@@ -3,10 +3,6 @@ read MY_NAME
 echo "hello, and welcome $MY_NAME"
 echo "creating a file called $MY_NAME"
 
-touch "$ {
-  MY_NAME
-}_file.txt"
-echo "file created for $MY_NAME" > "$ {
-  MY_NAME
-}_file.txt"
+touch "${MY_NAME}_file.txt"
+echo "file created for $MY_NAME" > "${MY_NAME}_file.txt"
 echo "done"
